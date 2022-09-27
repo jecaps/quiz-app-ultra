77bd3484835cbe14ff15f74c5dea45b9dc90313d
Jerika Kanz <jerikacapila@gmail.com>
2022-09-27 09:31:31 +0200

    Rename Files and Folders from Footer to Navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Card from "./components/card/Card";
 import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
+import Footer from "./components/navigation/Navigation";
 
 function App() {
   return (
