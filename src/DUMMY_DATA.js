288a2294baaa9caf0d39fe2f6e1bea25a9e14f03
Jerika Kanz <jerikacapila@gmail.com>
2022-09-27 17:38:35 +0200

    Create Dummy Data

--- src/DUMMY_DATA.js
+++ src/DUMMY_DATA.js
@@ -3,26 +3,26 @@ const DUMMY_DATA = [
     id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
     question: "What is the capital of Germany?",
     answer: "Berlin",
-    tags: ["Country", "Capital", "Germany"],
+    categories: ["Country", "Capital", "Germany"],
   },
   {
     id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
     question: "When did World War II end?",
     answer: "1945",
-    tags: ["WWWII", "History"],
+    categories: ["WWWII", "History"],
   },
   {
     id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
     question: "Which known city was destroyed by Mt. Vesuvius?",
     answer: "Pompeii",
-    tags: ["Ancient", "History", "Italy", "Volcano"],
+    categories: ["Ancient", "History", "Italy", "Volcano"],
   },
   {
     id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
     question:
       "How many years was Philippines under the colonial rule of Spain?",
     answer: "333 years",
-    tags: ["Colonialism", "History", "Spain", "Philippines"],
+    categories: ["Colonialism", "History", "Spain", "Philippines"],
   },
 ];
 
