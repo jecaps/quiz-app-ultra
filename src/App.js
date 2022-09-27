86fe85920f222c77e12c785b86493b8cce616225
Jerika Kanz <jerikacapila@gmail.com>
2022-09-27 12:37:48 +0200

    Add Props to Card Component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,36 @@ import Card from "./components/card/Card";
 import Header from "./components/header/Header";
 import Footer from "./components/navigation/Navigation";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Header />
       <main className="app__main">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          question={"What is the capital of Germany?"}
+          answer={"Berlin"}
+          tags={"Germany"}
+        />
+        <Card
+          question={"When did World War II end?"}
+          answer={"1945"}
+          tags={"history"}
+        />
+        <Card
+          question={"Which known city was destroyed by Mt. Vesuvius?"}
+          answer={"Pompeii"}
+          tags={"history"}
+          bookmarked
+        />
+        <Card
+          question={
+            "How many years was Philippines under the colonial rule of Spain?"
+          }
+          answer={"333 years"}
+          tags={"Philippines"}
+        />
       </main>
       <Footer />
     </div>
   );
 }
-
-export default App;
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,9 +1,9 @@
-export default function Card() {
+export default function Card({ question, answer, tags }) {
   return (
     <article className="card">
       <svg
         data-js="bookmark"
-        className="card__bookmark "
+        className="card__bookmark"
         xmlns="http://www.w3.org/2000/svg"
         x="0px"
         y="0px"
@@ -14,18 +14,14 @@ export default function Card() {
         <path d="M23,27l-8-7l-8,7V5c0-1.105,0.895-2,2-2h12c1.105,0,2,0.895,2,2V27z"></path>
       </svg>
 
-      <p className="card__question">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, maiores?
-      </p>
+      <p className="card__question">{question}</p>
       <a className="card__btn" href="./">
         Show Answer
       </a>
-      <p className="card__answer">The Answer</p>
+      <p className="card__answer">{answer}</p>
 
       <ul className="card__category">
-        <li className="card__item">#hmtl</li>
-        <li className="card__item">#css</li>
-        <li className="card__item">#flexbox</li>
+        <li className="card__item">#{tags}</li>
       </ul>
     </article>
   );
