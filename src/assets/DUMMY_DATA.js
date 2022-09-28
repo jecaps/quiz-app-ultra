13570d9d9c0e053290a523ba8f1526b3ecca871f
Jerika Kanz <jerikacapila@gmail.com>
2022-09-28 16:42:54 +0200

    Add property "bookmarked" to data

--- src/assets/DUMMY_DATA.js
+++ src/assets/DUMMY_DATA.js
@@ -4,18 +4,21 @@ const DUMMY_DATA = [
     question: "What is the capital of Germany?",
     answer: "Berlin",
     categories: ["Country", "Capital", "Germany"],
+    bookmarked: false,
   },
   {
     id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
     question: "When did World War II end?",
     answer: "1945",
     categories: ["WWII", "History"],
+    bookmarked: false,
   },
   {
     id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
     question: "Which known city was destroyed by Mt. Vesuvius?",
     answer: "Pompeii",
     categories: ["Ancient", "History", "Italy", "Volcano"],
+    bookmarked: false,
   },
   {
     id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
@@ -23,24 +26,28 @@ const DUMMY_DATA = [
       "How many years was Philippines under the colonial rule of Spain?",
     answer: "333 years",
     categories: ["Colonialism", "History", "Spain", "Philippines"],
+    bookmarked: true,
   },
   {
     id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
     question: "Who created JavaScript?",
     answer: "1995",
     categories: ["Programming", "JavaScript"],
+    bookmarked: true,
   },
   {
     id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
     question: "What is the tallest building of the world called?",
     answer: "Burj Khalifa",
     categories: ["Skyscrappers", "Building"],
+    bookmarked: false,
   },
   {
     id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
     question: "Who is the called the father of Modern Computer Science?",
     answer: "Alan Turing",
-    categories: ["Colonialism", "History", "Spain", "Philippines"],
+    categories: ["ComputerScience", "History"],
+    bookmarked: false,
   },
 ];
 
